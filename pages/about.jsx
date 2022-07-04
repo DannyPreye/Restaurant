d52c328bca377a18dd9a29aec374e8f9d207ab35
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-07-04 23:21:41 +0100

    finished about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Hero from "./components/Hero";
 import { bg } from "../public/Pics";
+import Booking from "./components/Booking";
+import Footer from "./components/Footer";
 
 const about = () => {
   return (
     <>
-      <Hero img="/assets/bg.png" />
+      <Hero
+        img="/assets/bg.png"
+        heading="ABOUT US"
+        subHeading={
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem pariatur nisi, quis porro, sed molestiae quae possimus dignissimos reiciendis rerum voluptates amet eligendi et sunt veniam, vitae dicta culpa? Corrupti."
+        }
+      />
+      <Booking img={bg} />
+      <Footer />
     </>
   );
 };
--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-function Hero({ img }) {
+function Hero({ img, heading, subHeading, children }) {
   return (
     <div
       className={`w-screen lg:h-[699px]    grid place-items-center relative`}
@@ -12,7 +12,16 @@ function Hero({ img }) {
         alt="alr"
         className="absolute top-0 left-0"
       />
-      <div className="w-full h-full absolute top-0 left-0 bg-[rgba(128,128,128,0.55)] z-10 backdrop-blur-sm "></div>
+      <div className="w-full h-full absolute top-0 left-0 bg-[rgba(128,128,128,0.81)] z-10 grid place-items-center  ">
+        <div className="w-[60%]">
+          <h1 className="font-inter text-orange text-[35px] leading-[42.36px] mb-[73px]">
+            {heading}
+          </h1>
+          <p className="font-[400] text-white text-[25px] leading-[30.26px]">
+            {subHeading}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
