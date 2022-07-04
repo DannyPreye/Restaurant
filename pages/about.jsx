de6cf6b05544b816afea77ca0b927bc8fe92a4d8
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-07-04 18:28:11 +0100

    working on the about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Hero from "./components/Hero";
+import { bg } from "../public/Pics";
 
 const about = () => {
   return (
     <>
-      <Hero />
+      <Hero img="/assets/bg.png" />
     </>
   );
 };
--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import HomeHero from "./HomeHero";
+import Image from "next/image";
 
-function Hero() {
+function Hero({ img }) {
   return (
     <div
-      className={`w-screen lg:w-[699px]  grid place-items-center relative`}
-    ></div>
+      className={`w-screen lg:h-[699px]    grid place-items-center relative`}
+    >
+      <Image
+        src={img}
+        layout="fill"
+        alt="alr"
+        className="absolute top-0 left-0"
+      />
+      <div className="w-full h-full absolute top-0 left-0 bg-[rgba(128,128,128,0.55)] z-10 backdrop-blur-sm "></div>
+    </div>
   );
 }
 
